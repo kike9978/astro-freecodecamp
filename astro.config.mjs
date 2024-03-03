45889ae30c6c20f30755c54b993f099dc6ce68ea
Kike <kike.mendoza9978@gmail.com>
2024-03-03 11:13:16 -0600

    Add SSR and deploy netilify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import pkg from 'typescript';
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
+const {
+  server
+} = pkg;
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  output: "server",
+  adapter: netlify()
 });
